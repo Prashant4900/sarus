123065e9859ea90571fd911d839e24f80750e85f
Prashant Nigam <prashantnigam490@gmail.com>
2025-06-16 01:13:32 +0530

    Refactor code structure for improved readability and maintainability

--- bricks/project/hooks/post_gen.dart
+++ bricks/project/hooks/post_gen.dart
@@ -3,26 +3,7 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  context.logger.info('Generate model schema...');
-
-  final result = Process.runSync(
-    'dart',
-    [
-      'run',
-      'build_runner',
-      'build',
-      '--delete-conflicting-outputs',
-    ],
-    workingDirectory: Directory.current.path,
-  );
-
-  if (result.exitCode == 0) {
-    context.logger.info('Model schema generated successfully.');
-  } else {
-    context.logger.err('Failed to generate model schema: ${result.stderr}');
-  }
-
-  context.logger.info('Running dart fix --apply...');
+  context.logger.progress('Running dart fix --apply...');
   final resultFix = Process.runSync(
     'dart',
     [
