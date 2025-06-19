85fbe5d4f320864d496d98440b2ad9bd77c4f292
Prashant Nigam <prashantnigam490@gmail.com>
2025-06-20 00:35:14 +0530

    feat: initialize example project with Docker setup, server implementation, and basic routing

---
+++ example/lib/hello/controllers.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:sarus/sarus.dart';
+
+part 'controllers.g.dart';
+
+@Controller('/hello')
+class HelloController extends BaseController {
+  HelloController() : super();
+
+  @Get('')
+  Future<Response> index(Request request) async {
+    return Response.ok(jsonEncode({'message': 'Hello from HelloController'}));
+  }
+
+  @override
+  RouterConfig get router => _$helloControllerRouterConfig(this);
+}
