fcf125f03bb3ae611f1bcafb3e9d567f9f4a89a9
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2025-06-22 23:25:33 +0530

    refactor router

--- bricks/project/__brick__/{{name}}/lib/config/router.dart
+++ bricks/project/__brick__/{{name}}/lib/config/router.dart
@@ -1,4 +1,6 @@
 import 'package:sarus/sarus.dart';
 import 'package:{{name}}/hello/controllers.dart';
 
-final router = Router(routes: [Route(controller: HelloController())]);
+final router = Router(routes: [
+    Route(controller: HelloController()),
+]);
