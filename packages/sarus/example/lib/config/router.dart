68974d7eefa6e100a9a56396b2cba3dbbccd521a
Prashant Nigam <prashantnigam490@gmail.com>
2025-06-26 12:22:51 +0530

    feat: add example application with routing, middleware, and server setup; update Makefile for main branch reference; include .gitignore and analysis options

---
+++ packages/sarus/example/lib/config/router.dart
@@ -0,0 +1,7 @@
+import 'package:example/greeting/endpoints.dart';
+import 'package:sarus/sarus.dart';
+
+final router = Router(
+  globalMiddleware: [logRequests()],
+  routes: [Route(endpoints: GreetingEndpoints())],
+);
